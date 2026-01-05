9842407530f1f9e17c6f9d8fc177330782cf9b32
Vishesh Gupta <visheshg990@gmail.com>
2026-01-05 18:20:21 +0530

    Improve error handling in searchLocation function
    Refactor searchLocation function to handle errors and update UI accordingly.

--- script.js
+++ script.js
@@ -11,15 +11,30 @@ async function gotPosition(position){
     searchLoction(longitude,latitude)
     
 }
-async function searchLoction(longitude,latitude){
-        const getData = await fetch(`http://api.weatherapi.com/v1/current.json?key=ff094cd206494d00bad115207252312&q=${latitude},${longitude}&aqi=yes`)
-        const data = await getData.json()
-        console.log(data)
-        loc.innerText=`You are currently at ${data.location.name} and in the region ${data.location.region} and in the country ${data.location.country}`
-        dateTime.innerText=`Current Date and current time : ${data.location.localtime} and Timezone : ${data.location.tz_id}`
-        temp.innerText=`Current Temperature is : ${data.current.temp_c} , feels like :${data.current.feelslike_c} & Humidity is : ${data.current.humidity}`
-        latlong.innerText = `Current Latitude: ${latitude} & Longitude: ${longitude}`
-    } 
+async function searchLoction(longitude, latitude) {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=ff094cd206494d00bad115207252312&q=${latitude},${longitude}&aqi=yes`
+    );
+
+    if (!response.ok) {
+      throw new Error("Weather API blocked");
+    }
+
+    const data = await response.json();
+    console.log("API DATA:", data);
+
+    loc.innerText = `You are currently at ${data.location.name}, ${data.location.region}, ${data.location.country}`;
+    dateTime.innerText = `Current Date & Time: ${data.location.localtime}`;
+    temp.innerText = `Current Temperature: ${data.current.temp_c}°C`;
+
+  } catch (error) {
+    console.error(error);
+    loc.innerText = "Weather data not accessible on deployed site ❌";
+    dateTime.innerText = "";
+    temp.innerText = "";
+  }
+}
 function failedToGet(){
     console.log("Not able to get location")
 }
